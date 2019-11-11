665acd3717e58ea833dc098e2721f8bf83a9453f
Andrew Pearson <andrewpearson@wintermute.lan>
2019-11-11 19:25:56 +0000

    Initial Commit

---
+++ responsive_sidebar_menu.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * @package   ANDYP - Responsive Sidebar Menus
+ * @author    Andy Pearson <andy@londonparkour.com>
+ * @copyright 2020 LondonParkour
+ * 
+ * @wordpress-plugin
+ * Plugin Name:       _ANDYP - Responsive Sidebar Menus
+ * Plugin URI:        http://londonparkour.com
+ * Description:       Either a dynamic sidebar or a select dropdown, depending on width media query
+ * Version:           1.0.0
+ * Author:            Andy Pearson
+ * Author URI:        https://londonparkour.com
+ * Domain Path:       /languages
+ */
+
